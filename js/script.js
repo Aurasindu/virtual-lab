0e582760d56344e352964d24a3f56cd32b066c04
Aurasindu <aurasindu@gmail.com>
2024-10-24 00:55:50 +0700

    yuk dikit lg add content tp turu dl

--- js/script.js
+++ js/script.js
@@ -6,23 +6,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Menyimpan jawaban benar
         const correctAnswers = {
-            q1: "Huruf besar",
-            q2: "Tanda titik"
+            q1: "bika-ambon",
+            q2: "sub-bagian",
+            q3: "ultrasound"
         };
 
         // Memeriksa jawaban
         const q1Answer = document.querySelector('input[name="q1"]:checked')?.value;
         const q2Answer = document.querySelector('input[name="q2"]:checked')?.value;
+        const q3Answer = document.querySelector('input[name="q3"]:checked')?.value;
 
-        // Menampilkan umpan balik untuk pertanyaan 1
         const feedbackQ1 = document.getElementById('feedback-q1');
         feedbackQ1.textContent = (q1Answer === correctAnswers.q1) ? 'Benar!' : 'Salah!';
-        feedbackQ1.className = (q1Answer === correctAnswers.q1) ? '' : 'error'; // Tambahkan kelas 'error' jika salah
+        feedbackQ1.className = (q1Answer === correctAnswers.q1) ? '' : 'error';
 
-        // Menampilkan umpan balik untuk pertanyaan 2
         const feedbackQ2 = document.getElementById('feedback-q2');
         feedbackQ2.textContent = (q2Answer === correctAnswers.q2) ? 'Benar!' : 'Salah!';
-        feedbackQ2.className = (q2Answer === correctAnswers.q2) ? '' : 'error'; // Tambahkan kelas 'error' jika salah
+        feedbackQ2.className = (q2Answer === correctAnswers.q2) ? '' : 'error';
+
+        const feedbackQ3 = document.getElementById('feedback-q3');
+        feedbackQ3.textContent = (q3Answer === correctAnswers.q3) ? 'Benar!' : 'Salah!';
+        feedbackQ3.className = (q3Answer === correctAnswers.q3) ? '' : 'error';
     });
 });
 
